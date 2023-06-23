c2b868fc25999797f32f9f81cf56a70169c6b2ad
Arul Kumarc <arulk1535@gmail.comc>
2023-06-22 22:43:04 -0300

    newreg

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -60,7 +60,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">CHAT BOX</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
