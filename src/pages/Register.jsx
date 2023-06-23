cf72816c7a414413f6247028c555f032d2f80642
Arul Kumarc <arulk1535@gmail.comc>
2023-06-22 22:55:56 -0300

    newregce

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -76,7 +76,7 @@ const Register = () => {
           {err && <span>Something went wrong</span>}
         </form>
         <p>
-          You do have an account? <Link to="/Register">Login</Link>
+          You do have an account? <Link to="/login">Login</Link>
         </p>
       </div>
     </div>
